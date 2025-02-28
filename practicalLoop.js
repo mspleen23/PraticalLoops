4834c90dce289e802d96cdb2eaaf691b8190a094
Melissa <mespleen@gmail.com>
2025-02-27 21:49:40 -0500

    Loops Part 1

--- practicalLoop.js
+++ practicalLoop.js
@@ -1,11 +1,18 @@
-// Fizz Bizz
-for (let i =1; i <=10; i++){
-        if (i %3 ==0){
-          console.log("Fizz")}
-        }esle if (i %5 ==0) {
-            console.log ("Buzz")
-        }else if (i% 3 && i%5) {
-            console.log ("Fizz Bizz")
-        }defualt {
-            console.log ("i)")
+//  Part 1: Fizz Bizz
+for (let i =1; i <=100; i++){
+        if (i %3 == 0){
+          console.log("Fizz");
+        } else if (i %5 ==0) {
+            console.log ("Buzz");
+        } else if (i% 3==0 && i%5==0){
+            console.log ("Fizz Buzz");
+        }else {
+            console.log (i);
         }
+        }
+
+ // Part 2: Prime Time 
+
+ 
+    
+
