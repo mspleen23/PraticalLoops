631d25637de35c235d727a2739956688e98079d6
Melissa <mespleen@gmail.com>
2025-02-27 21:30:57 -0500

    Pratical Loops

--- practicalLoop.js
+++ practicalLoop.js
@@ -1,1 +1,11 @@
-gi
+// Fizz Bizz
+for (let i =1; i <=10; i++){
+        if (i %3 ==0){
+          console.log("Fizz")}
+        }esle if (i %5 ==0) {
+            console.log ("Buzz")
+        }else if (i% 3 && i%5) {
+            console.log ("Fizz Bizz")
+        }defualt {
+            console.log ("i)")
+        }
